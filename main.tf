684b2ebf185782264293a6d78bf69eaa4e45d1e0
Gerrit Scherpenzeel <gerrit@Gerrits-MacBook-Pro.local>
2025-03-11 08:16:34 +0100

    initial commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.15.0"
+      version = ">=4.15.0"
     }
   }
 }
@@ -10,9 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "c064671c-8f74-4fec-b088-b53c568245eb"
-  resource_provider_registrations {
-    enabled = false
-  }
+  skip_provider_registration = true
 }
 
 # Use existing resource group
